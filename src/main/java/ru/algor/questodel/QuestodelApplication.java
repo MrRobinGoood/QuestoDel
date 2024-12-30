63b61e707d95dfca138ed0fe5c838f17a1428b5b
Andrey Bartenev <59123372+MrRobinGoood@users.noreply.github.com>
2024-12-31 00:09:38 +0400

    Initial commit

---
+++ src/main/java/ru/algor/questodel/QuestodelApplication.java
@@ -0,0 +1,13 @@
+package ru.algor.questodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestodelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestodelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/algor/questodel/QuestodelApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.algor.questodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestodelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
